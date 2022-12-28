863a1dc32ba8eb616d3ad12435b2742c93e9ccac
Hemanth Raj <hemanthraj@Hemanths-MacBook-Pro.local>
2022-12-28 15:01:39 +0530

    space added

--- src/features/FocusHistory.js
+++ src/features/FocusHistory.js
@@ -3,7 +3,6 @@ import { Text, View, StyleSheet, FlatList } from 'react-native';
 import { colors } from '../utils/colors';
 import { fontSizes, spacing } from '../utils/sizes';
 
-
 export const FocusHistory = ({history}) => {
     if(!history || !history.length) return <Text style={styles.title}>We haven't focused on anything yet! </Text>
     const renderItem = ({item}) => <Text style={styles.item}> - {item}</Text>
